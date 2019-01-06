847c0579b41ff86397a4d1d8471758c7c38db58e
Melinda Hardin <melindawad@gmail.com>
2019-01-06 17:11:40 -0600

    back

---
+++ client/src/components/HelloDiv.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HelloDiv=() =>(
+	<div>
+		<h1> Hello Wooooorld!</h1>
+		<h2> Things I like:</h2>
+		<ul>
+			<li>Ellie</li>
+			<li>Netflix</li>
+			<li>Popcorn</li>
+		</ul>
+	</div>
+)
+export default HelloDiv
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import ReqButton from "./components/ReqButton"
-import Main from "./components/Main";
-
-const App = () => <Main />;
-
-
-
-export default App;

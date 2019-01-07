4fe2af1e4254c68365c1414ed4c585c725d5b840
Melinda Hardin <melindawad@gmail.com>
2019-01-06 18:21:22 -0600

    front and back communicating

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const path = require ("path");
 const PORT = process.env.PORT || 3001
 const app = express(); 
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static("client/build"));
-}
+app.use(express.static("client/build"));
+
 
 app.get("*", function(req, res){
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
